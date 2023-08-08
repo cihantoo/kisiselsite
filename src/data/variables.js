da4f31edb4a81a27662280f6124d2f275214ff96
Cihan Yüksel <66547306+cihantoo@users.noreply.github.com>
2023-08-08 16:26:52 +0300

    Update variables.js

--- src/data/variables.js
+++ src/data/variables.js
@@ -1,12 +1,12 @@
 export default {
 
-    userId: "642752306441617417",
-    github: "justnyde",
-    description: "My name is Just Nyde. I am 16 years old Turkish student. I interested in Software and Design. I can develop front-end and back-end websites for now.",
+    userId: "722018893115162676",
+    github: "cihantoo",
+    description: "My name is Cihan. I am 17 years old Turkish student. I interested in Software and Design. I can develop front-end and back-end websites for now.",
     about: {
-        description: "Hello. My name is <span class='text-primary'>Just Nyde</span>. I am <span class='text-primary'>16 years</span> old. I am a <span class='text-primary'>3rd year student</span> and I interested in <span class='text-primary'>Software</span>. I've been coding <span class='text-primary'>since 2019</span> and I want to improve myself in this field.",
-        liveIn: "İstanbul, Küçükçekmece",
-        birthday: "16 October, 2006",
+        description: "Hello. My name is <span class='text-primary'>Cihan</span>. I am <span class='text-primary'>17 years</span> old. I am a <span class='text-primary'>4th class student @ Tosya Cumhuriyet Anatolian High School</span> and I'm interested in <span class='text-primary'>Software & Translating</span>. I've been coding <span class='text-primary'>since 2019</span> and I want to improve myself in this field.",
+        liveIn: "Çorum, Kargı",
+        birthday: "09 April, 2006",
         gender: "Man"
     }
 
